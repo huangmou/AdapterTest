e5d1572ca927a4bd507d04713fd823d1744f9f7e
huangjunmou <13661398952@163.com>
2015-12-08 10:12:24 +0800

    My*************

--- app/src/main/java/com/example/dell/adaptertest/adapter/Myadapter.java
+++ app/src/main/java/com/example/dell/adaptertest/adapter/Myadapter.java
@@ -2,6 +2,7 @@ package com.example.dell.adaptertest.adapter;
 
 import android.content.Context;
 
+import com.example.dell.adaptertest.Entity.TestInfo;
 import com.example.dell.adaptertest.R;
 import com.example.dell.adaptertest.base.CommonAdapter;
 import com.example.dell.adaptertest.base.ViewHolder;
@@ -21,10 +22,9 @@ public class Myadapter extends CommonAdapter {
 
     @Override
     public void convert(ViewHolder helper, Object item) {
-        helper.getView(R.id.textView_name);
-        helper.getView(R.id.textView_adress);
-        helper.getView(R.id.textView_time);
-        System.out.println("sfsdf");
+        helper.setText(R.id.textView_name,((TestInfo)item).getName());
+        helper.setText(R.id.textView_adress, ((TestInfo) item).getAdress());
+        helper.setText(R.id.textView_time,((TestInfo)item).getTime());
 
 
 
